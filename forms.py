1cff8742afb8d6a5c5e11c49f37cbf44fb8c0c1b
rkumar-ravi21 <rkumar.ravi21@gmail.com>
2020-03-20 00:15:40 +0530

    5 functions added

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, Form
 from wtforms import StringField, PasswordField, RadioField, DecimalField
 from wtforms.validators import DataRequired, Email
+from wtforms import SelectField
 from pymongo import MongoClient, errors
 
 
@@ -17,3 +18,27 @@ class RegisterForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+
+class BloodDonateForm(FlaskForm):
+    bg = SelectField('BloodGroup', choices = [('ap', 'A(+ve)'), ('an', 'A(-ve)'), ('bp', 'B(+ve)'), ('bn', 'B(-ve)'), ('abp', 'AB(+ve)'), ('abn', 'AB(-ve)'), ('op', 'O(+ve)'), ('on', 'O(-ve)')], validators=[DataRequired()])
+    NoOfUnits = SelectField('NoOfUnits', choices = [('2', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')], validators=[DataRequired()])
+
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField('Old Password', validators=[DataRequired()])
+    new_password = PasswordField('New Password', validators=[DataRequired()])
+
+class UpdateProfileForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    gendre = RadioField('Gendre', choices=[('Male', 'Male'), ('Female', 'Female'), ('Others', 'Others')],  validators=[DataRequired()])
+    age = StringField('Age', validators=[DataRequired()])
+    mobile = StringField('Mobile', validators=[DataRequired()])
+
+class SearchBloodForm(FlaskForm):
+    bg = SelectField('BloodGroup', choices=[('ap', 'A(+ve)'), ('an', 'A(-ve)'), ('bp', 'B(+ve)'), ('bn', 'B(-ve)'), ('abp', 'AB(+ve)'), ('abn', 'AB(-ve)'), ('op', 'O(+ve)'), ('on', 'O(-ve)')], validators=[DataRequired()])
+
+class BloodRequestForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    gendre = RadioField('Gendre', choices=[('Male', 'Male'), ('Female', 'Female'), ('Others', 'Others')],  validators=[DataRequired()])
+    age = StringField('Age', validators=[DataRequired()])
+    mobile = StringField('Mobile', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
--- runner.py
+++ runner.py
@@ -1,6 +1,6 @@
 
-from flask import Flask, render_template, redirect, url_for, request
-from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
 from flask_wtf.csrf import CSRFProtect
 from pymongo import MongoClient, errors
 from hashlib import sha512
@@ -9,7 +9,7 @@ from forms import *
 
 
 try:
-    client = MongoClient("mongodb://localhost:27017/")
+    client = MongoClient("mongodb://localhost:27018/")
     database = client["blood_bank"]
 except errors.ServerSelectionTimeoutError:
     print("Cannot connect to mongo server.")
@@ -89,20 +89,41 @@ def register():
 @app.route("/blooddonated", methods=["POST", "GET"])
 @login_required
 def donate_blood():
-    return render_template('blooddonated.html')
+    form = BloodDonateForm()
+
+    if(request.method == 'POST'):
+        database["blood_detail"].insert_one(form.data)
+        return redirect('/dfg2')
+    return render_template('blooddonated.html', form = form)
 
 
 @app.route("/changepwd", methods=["POST", "GET"])
 @login_required
 def change_password():
-    return render_template('changepwd.html', message="Password changed successfully")
+    userid = current_user.id
+    form = ChangePasswordForm()
+
+    if(request.method == 'POST'):
+        np = { "$set" : {"password": sha512(form.new_password.data.encode()).hexdigest() }}
+        for val in database["user"].find({"_id": userid}, {"_id":0, "password":1}):
+            if ( val["password"] == sha512(form.old_password.data.encode()).hexdigest() ):
+                database["user"].update_one(val, np)
+                return render_template('changepwd.html', form=form, message="Password changed successfully")
+    return render_template('changepwd.html', form=form, error = "Some error occured")
 
 
 @app.route("/updatepf", methods=["POST", "GET"])
 @login_required
 def update_profile():
-    form = RegisterForm()
-    return render_template('updatepf.html')
+    form = UpdateProfileForm()
+    userid = current_user.id
+
+    if(request.method == 'POST'):
+        val2 = { "$set" : {"name":form.name.data, "gender":form.gendre.data, "age":form.age.data, "mobile":form.mobile.data}}
+        for val in database["user"].find({"_id":userid}, {"_id":0, "name":1, "gender":1, "age":1, "mobile":1}):
+            database["user"].update_one(val, val2)
+            return redirect('/updatepf')
+    return render_template('updatepf.html', form = form, message = "Some error occurred")
 
 
 @app.route("/logout")
@@ -111,6 +132,26 @@ def logout():
     logout_user()
     return redirect("/")
 
+
+@app.route("/searchblood", methods=["POST", "GET"])
+def searchblood():
+    form = SearchBloodForm()
+
+    if(request.method == 'POST'):
+        for val in database["blood_detail"].find({"bg":form.bg.data}, {"_id":0, "NoOfUnits":1}):
+            print(val) #show data in table format & redirect to bloodrequest.html (blood-request-form)
+            return render_template("/searchblood.html", form=form, message = "data found")
+    return render_template("/searchblood.html", form=form, message = "No data found")
+
+
+@app.route("/bloodrequest", methods=["POST", "GET"])
+def bloodrequest():
+    form = BloodRequestForm()
+
+    if(request.method == 'POST'):
+        database["blood_request"].insert_one(form.data)
+        return render_template("/bloodrequest.html", form=form, message = "We will get back to you soon...")
+    return render_template("/bloodrequest.html", form=form, message = "Some error occured")
 
 @app.route("/<file>")
 def render_file(file):
